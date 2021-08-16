141611b4647c126d5c7ee625f5cbc52ef00a4156
Kerwin <kerwin2821@gmail.com>
2021-08-16 14:10:02 -0400

    Test

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+module HSMComunicatorClient {
+	exports com.alodiga.hsm.access.conection;
+	exports com.alodiga.hsm.response;
+	exports com.alodiga.hsm.data.object;
+	exports com.alodiga.hsm;
+	exports com.alodiga.hsm.exception;
+	exports com.alodiga.hsm.data;
+	exports com.alodiga.hsm.util;
+	exports com.alodiga.hsm.access.data;
+	exports Framework;
+	exports com.alodiga.hsm.operations;
+
+	requires java.desktop;
+	requires java.logging;
+	requires java.sql;
+}
